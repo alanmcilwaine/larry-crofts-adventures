1e57b12a9e63eb21cdecefaafd5a0c8a7bef1334
YL <yl@apple.com>
2024-09-21 23:04:14 +1200

    Optimise LockedExit Style

--- src/nz/ac/wgtn/swen225/lc/domain/GameItem/LockedExit.java
+++ src/nz/ac/wgtn/swen225/lc/domain/GameItem/LockedExit.java
@@ -9,7 +9,6 @@ import nz.ac.wgtn.swen225.lc.domain.Interface.Item;
 import nz.ac.wgtn.swen225.lc.domain.Tile;
 
 import java.util.Collection;
-import java.util.NoSuchElementException;
 import java.util.logging.Level;
 
 public record LockedExit() implements Item, GameStateObserver {
@@ -25,25 +24,25 @@ public record LockedExit() implements Item, GameStateObserver {
     /**
      * LockedExit item will observe game state and receive
      * an update and change accordingly.
+     *
      * @param gameState game state.
      */
     @Override
     public void update(GameState gameState) {
-        var playerTreasure = gameState.player().getTreasure()
-                .stream().filter(e -> e instanceof Treasure).toList().size();
-        var self = this;
-        try {
-            Tile<Item> tile = gameState.board().stream()
-                    .flatMap(Collection::stream)
-                    .filter(x->x.item instanceof LockedExit)
-                    .toList()
-                    .getFirst();
-            if (playerTreasure == GameBoard.totalTreasure) {
-                tile.item = new Exit();
-            }
-        } catch (NoSuchElementException e) {
-            DomainLogger.LOGGER.getLogger().log(Level.INFO, "Exit is already unlocked.");
-            GameBoard.unsubscribeGameState(self);
-        }
+        long playerTreasure = gameState.player().getTreasure()
+                .stream().filter(e -> e instanceof Treasure).count();
+
+        gameState.board().stream()
+                .flatMap(Collection::stream)
+                .filter(x -> x.item instanceof LockedExit)
+                .findFirst()
+                .ifPresentOrElse(tile -> {
+                    if (playerTreasure == GameBoard.totalTreasure) {
+                        tile.item = new Exit();
+                    }
+                }, () -> {
+                    DomainLogger.LOGGER.getLogger().log(Level.INFO, "Exit is already unlocked.");
+                    GameBoard.unsubscribeGameState(this);
+                });
     }
 }
