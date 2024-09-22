d5ec9e7df3520a89ff93da8dd84ed3cc86b75844
John Rais <username@ecs.vuw.ac.nz>
2024-09-22 15:30:38 +1200

    #34 Made keys neatly remember when two opposite keys are pressed and released
    This means that if you hit UP, keep holding it, then press and release DOWN, then the character will start moving up again.

--- src/nz/ac/wgtn/swen225/lc/app/Controller.java
+++ src/nz/ac/wgtn/swen225/lc/app/Controller.java
@@ -1,28 +1,37 @@
 package nz.ac.wgtn.swen225.lc.app;
 
 import java.awt.event.KeyEvent;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+
+import nz.ac.wgtn.swen225.lc.app.Keys;
 
 class Controller extends Keys{
     //Default keys
-    // TODO Store a runnable in here, as a reference to the last thing pressed.
+
     enum Action{
-        UP,
-        DOWN,
-        LEFT,
-        RIGHT;
+        UP(KeyEvent.VK_W),
+        DOWN(KeyEvent.VK_S),
+        LEFT(KeyEvent.VK_A),
+        RIGHT(KeyEvent.VK_D);
+
+        int key; Action opposite; Runnable stop = () -> {};
+
+        static{
+            UP.opposite = DOWN;
+            DOWN. opposite = UP;
+            LEFT.opposite = RIGHT;
+            RIGHT.opposite = LEFT;
+        }
+        Action(int key){this.key = key;}
     }
-    static HashMap<Action, Integer> bindings = new HashMap<>(Map.of(
-            Action.UP,KeyEvent.VK_W,
-            Action.DOWN,KeyEvent.VK_S,
-            Action.LEFT,KeyEvent.VK_A,
-            Action.RIGHT,KeyEvent.VK_D
-    ));
+
     Controller(){
-        setAction(bindings.get(Action.UP), () -> currentCommand = Command.Up, () -> currentCommand = Command.None);
-        setAction(bindings.get(Action.DOWN), () -> currentCommand = Command.Down, () -> currentCommand = Command.None);
-        setAction(bindings.get(Action.LEFT), () -> currentCommand = Command.Left, () -> currentCommand = Command.None);
-        setAction(bindings.get(Action.RIGHT), () -> currentCommand = Command.Right, () -> currentCommand = Command.None);
+        setAction(Action.UP, () -> currentCommand = Command.Up);
+        setAction(Action.DOWN, () -> currentCommand = Command.Down);
+        setAction(Action.LEFT, () -> currentCommand = Command.Left);
+        setAction(Action.RIGHT, () -> currentCommand = Command.Right);
+
+        //set all the stop functions.
+        Arrays.stream(Action.values()).forEach(action -> action.stop = noCommand);
     }
 }
--- src/nz/ac/wgtn/swen225/lc/app/Keys.java
+++ src/nz/ac/wgtn/swen225/lc/app/Keys.java
@@ -9,11 +9,28 @@ import javax.swing.SwingUtilities;
 
 class Keys implements KeyListener {
     protected Command currentCommand = Command.None;
+    protected final Runnable noCommand = () ->{currentCommand = Command.None;};
     private final Map<Integer,Runnable> actionsPressed= new HashMap<>();
     private final Map<Integer,Runnable> actionsReleased= new HashMap<>();
-    public void setAction(int keyCode, Runnable onPressed, Runnable onReleased){
-        actionsPressed.put(keyCode, onPressed);
-        actionsReleased.put(keyCode, onReleased);
+
+    /**
+     * This will add a EntrySet to the actionsPressed and actionsReleased maps that will be executed when keys are pressed
+     * actionsPressed -> runs the onPressed Runnable passed in, then sets the opposite key (opposite of UP is DOWN) to do
+     * this keys onPressed Runnable.
+     * This way, if you hold down UP, then without letting go of UP, hold down DOWN, when you let go of DOWN, UP's runnable
+     * will run, resulting in the player moving upwards again.
+     *
+     * actionsReleased -> runs the action.stop, which either sets currentCommand to NONE, or runs the opposite keys runnable
+     * (see above)
+     *
+     * @param action What action are we currently setting up
+     * @param onPressed What should happen when we press this actions key?
+     * @auther John Rais
+     */
+    public void setAction(Controller.Action action, Runnable onPressed){
+        actionsPressed.put(action.key, () -> {onPressed.run(); action.opposite.stop = onPressed;});
+        //On released depends on what other keys you are holding down, so it can change
+        actionsReleased.put(action.key, () -> {action.stop.run(); action.opposite.stop = noCommand;});
     }
     public void keyTyped(KeyEvent e){}
     public void keyPressed(KeyEvent e){
