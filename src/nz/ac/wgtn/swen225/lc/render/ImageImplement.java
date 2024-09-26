a4c5b6c01fa7f6e5d516a443049ae31ea45eb091
SarahLi <libaix@ecs.vuw.ac.nz>
2024-09-26 23:59:07 +1200

    create the Graphic parameter in drawing method

--- src/nz/ac/wgtn/swen225/lc/render/ImageImplement.java
+++ src/nz/ac/wgtn/swen225/lc/render/ImageImplement.java
@@ -37,16 +37,16 @@ public class ImageImplement{
     /**
      * draw the image in each game board to the jpanel.
      */
-    public void drawImages(GameState gameState){
+    public void drawImages(GameState gameState, Graphics g){
         new BackgroundImplement().drawBackGround(jpanel);
-        drawItemsTile(gameState);
-        drawActors(gameState);
+        drawItemsTile(gameState, g);
+        drawActors(gameState, g);
     }
 
     /**
      * draw all the items and the tiles
      */
-    public void drawItemsTile(GameState gameState){
+    public void drawItemsTile(GameState gameState, Graphics g){
         List<List<Tile<Item>>> gameBoard = gameState.board();
         Player player = gameState.player();
         // run through all the tiles
@@ -55,7 +55,7 @@ public class ImageImplement{
             drawOneImage("Tile",
                     tile.location.x() - player.getLocation().x(),
                     tile.location.y() - player.getLocation().y(),
-                    jpanel);
+                    jpanel, g);
 
             // items on the tile
             listTile.stream()
@@ -63,7 +63,7 @@ public class ImageImplement{
                     .forEach(t -> drawOneImage(t.getItemOnTile(),
                             t.location.x() - player.getLocation().x(),
                             t.location.y() - player.getLocation().y(),
-                            jpanel));
+                            jpanel, g));
         }));
 
     }
@@ -71,24 +71,23 @@ public class ImageImplement{
     /**
      * draw all the Actors
      */
-    public void drawActors(GameState gameState){
+    public void drawActors(GameState gameState, Graphics g){
         // player
         Player player = gameState.player();
-        drawOneImage(player.toString(), 0, 0, jpanel);
+        drawOneImage(player.toString(), 0, 0, jpanel, g);
 
         // robots
         List<Robot> robots = gameState.robots();
         robots.forEach(robot -> drawOneImage(robot.toString(),
                 robot.getLocation().x() - player.getLocation().x(),
-                robot.getLocation().y() - player.getLocation().y(), jpanel));
+                robot.getLocation().y() - player.getLocation().y(), jpanel, g));
 
     }
 
     /**
      * draw one image
      */
-    public void drawOneImage(String imageName, int x, int y, JPanel jpanel){
-        Graphics g = jpanel.getGraphics();
+    public void drawOneImage(String imageName, int x, int y, JPanel jpanel, Graphics g){
         g.drawImage(Img.INSTANCE.getImgs(imageName + ".png"), (x + BUFFER_SIZE) * IMAGE_SIZE,
                 (y + BUFFER_SIZE) * IMAGE_SIZE, jpanel);
 
