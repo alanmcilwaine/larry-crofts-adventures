297838af0d4f17694b974f9fcf32639fd77f89f3
Carla <carlaparinas1231@gmail.com>
2024-10-03 05:34:09 +1300

    added new files for new items

---
+++ src/nz/ac/wgtn/swen225/lc/domain/GameItem/Crate.java
@@ -0,0 +1,11 @@
+package nz.ac.wgtn.swen225.lc.domain.GameItem;
+
+import nz.ac.wgtn.swen225.lc.domain.GameActor.MovableBox;
+import nz.ac.wgtn.swen225.lc.domain.Utilities.Location;
+
+public class Crate extends MovableBox {
+
+  public Crate(Location location) {
+    super(location);
+  }
+}
---
+++ src/nz/ac/wgtn/swen225/lc/domain/GameItem/Laser.java
@@ -0,0 +1,6 @@
+package nz.ac.wgtn.swen225.lc.domain.GameItem;
+
+import nz.ac.wgtn.swen225.lc.domain.Interface.Item;
+
+public class Laser implements Item {
+}
