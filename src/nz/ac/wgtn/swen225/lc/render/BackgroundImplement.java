171d278281b820c8091847048fadf99fe067fc53
SarahLi <libaix@ecs.vuw.ac.nz>
2024-10-07 15:58:02 +1300

    update the way to load the image, make it more reliable

--- src/nz/ac/wgtn/swen225/lc/render/BackgroundImplement.java
+++ src/nz/ac/wgtn/swen225/lc/render/BackgroundImplement.java
@@ -6,23 +6,34 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class BackgroundImplement {
-    private ImageIcon gifImage;
+    private Image gifImage;
 
     public BackgroundImplement() {
-        try {
-            gifImage = new ImageIcon(getClass().getResource("/BackgroundImage/bc.gif"));
 
-        } catch (RuntimeException e){
-            throw new Error("Failed to load background: " + gifImage.getImage());
+        File backgroundFolder = new File("BackgroundImage");
+        File[] imageFiles = backgroundFolder.listFiles();
+        assert imageFiles != null;
+        for(File file: imageFiles){
+            try{
+                if(file.getName().endsWith(".gif")) {
+                    ImageIcon imageIcon = new ImageIcon(file.getAbsolutePath());
+                    gifImage = imageIcon.getImage();
+                }
+            }
+            catch (RuntimeException e){
+                throw new Error("Failed to load image: " + file.getName());
+            }
+
         }
     }
 
 
     public void drawBackGround(JPanel jPanel, Graphics g) {
 
-        g.drawImage(gifImage.getImage(), 0, 0, jPanel.getWidth(), jPanel.getHeight(), jPanel);
+        g.drawImage(gifImage, 0, 0, jPanel.getWidth(), jPanel.getHeight(), jPanel);
 
     }
 }
